7e830db2f02a028882fbe55b4c3cd6f5eb10bdff
yuri-italo <yuri.italo94@gmail.com>
2023-01-04 09:58:30 -0300

    WebAdapter pom fixed

--- web-adapter/src/test/java/br/com/gubee/webadapter/CompareHeroControllerTest.java
+++ web-adapter/src/test/java/br/com/gubee/webadapter/CompareHeroControllerTest.java
@@ -18,7 +18,6 @@ import java.util.UUID;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(CompareHeroController.class)
 @ContextConfiguration(classes = CompareHeroController.class)
